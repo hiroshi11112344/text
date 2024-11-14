14e167ce8f8bd2ff37fcf16c63056a71e0b6628f
hiroshi11112344 <hikikaeru0303@icloud.com>
2024-11-14 18:52:01 +0900

    11/14修正

--- db/migrate/20241006113100_add_to_totaltime_to_quizzes.rb
+++ db/migrate/20241006113100_add_to_totaltime_to_quizzes.rb
@@ -1,5 +1,8 @@
 class AddToTotaltimeToQuizzes < ActiveRecord::Migration[7.2]
   def change
-    add_column :quizzes, :total_time, :float, from: nil, to: "0"
+    #add_column :quizzes, :total_time, :float, from: nil, to: "0"
+    #↓に書き換え　１１/14
+    add_column :quizzes, :total_time, :float, default: 0
+
   end
 end
--- db/migrate/20241006115334_add_to_totaltime_to_users.rb
+++ db/migrate/20241006115334_add_to_totaltime_to_users.rb
@@ -1,6 +1,9 @@
 class AddToTotaltimeToUsers < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :user_, :string
-    add_column :users, :total_time, :float, from: nil, to: "0"
+    #add_column :users, :total_time, :float, from: nil, to: "0"
+         #↓に書き換え　１１/14
+    add_column :users, :total_time, :float, default: 0
+
   end
 end
