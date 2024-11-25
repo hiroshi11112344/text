779d3d26b0e66625a8dc77a778330d7dabcd30c2
hiroshi11112344 <hikikaeru0303@icloud.com>
2024-11-25 16:24:10 +0900

    送られてきた値をセーブでコードを書いていた所をupdateに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,35 +18,47 @@ class ApplicationController < ActionController::Base
   def quiz_check_answer
     @quiz = Quiz.find_by(id: params[:id])
     selected_answer = params[:selected_answer]
-    @user = @current_user
+    @current_user
     # ↑変数入れなくて良い
     if @quiz && selected_answer
-      if @user 
+      if @current_user
         elapsed_time = (Time.current - @quiz.started_at).to_i # 経過時間（秒単位）
         # 秒数を時間、分、秒の形式に変換
         time_in_hours = Time.at(elapsed_time).utc.strftime("%H:%M:%S")
 
         # 中間テーブルログインしているユーザーIDと開いているクイズIDの紐付け
-        quiz_result = QuizResult.find_or_create_by(user: @user, quiz: @quiz) 
-        # セーブをアップデート文に買える
+        quiz_result = QuizResult.find_or_create_by(user: @current_user, quiz: @quiz) 
+        
         if selected_answer == @quiz.answer.user_answer
-          # QuizResultに保存（ユーザーとクイズごとの解答時間を管理）
+          
+          # 中間テーブル
+          #毎回０にして新しいデータを保存するので　if 小さい　＞　大きい　で保存する時の条件をつける
           quiz_result.time_spent = 0
           quiz_result.time_spent = elapsed_time.to_i
           quiz_result.save
 
+          # QuizResultに保存（ユーザーとクイズごとの解答時間を管理）
+          # セーブをアップデート文に買える　１１２５
+          @current_user.update(
+             # ユーザーに正解したらスコア+1
+            score: @current_user.score.to_i + 1,   # ユーザーに正解したらスコア+1
+
+            # total_time が nil の場合は 0 に設定してから秒単位で加算
+            total_time: (@current_user.total_time || 0.0) + elapsed_time.to_i  
+          )
+        
+          
           # ユーザーに正解したらスコア+1
-          @user.score = @user.score.to_i + 1
-          @user.save # スコアをデータベースに保存  
-          flash[:notice] = "正解 : #{time_in_hours}"
-            
-
+          #@user.score = @user.score.to_i + 1
+          #@user.save # スコアをデータベースに保存  
+          #flash[:notice] = "正解 : #{time_in_hours}"
           # total_time が nil の場合は 0 に設定してから秒単位で加算
-          @user.total_time ||= 0.0
-          @user.total_time += elapsed_time.to_i # 秒単位で加算
-          @user.save
-
+          #@user.total_time ||= 0.0
+          #@user.total_time += elapsed_time.to_i # 秒単位で加算
+          #@user.save
+          
           redirect_to("/quizzes/new")
+          
         else selected_answer != @quiz.answer.user_answer
           flash[:notice] = "不正解"
           redirect_to("/quizzes/new")
